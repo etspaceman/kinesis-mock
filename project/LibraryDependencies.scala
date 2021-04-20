d8cfa9fbdddfb52b4d074733201b6fbb3fbb60ee
Scala Steward <me@scala-steward.org>
2021-04-20 21:02:27 +0200

    Update svm to 21.1.0

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -9,7 +9,7 @@ object LibraryDependencies {
   val JaxbApi = "javax.xml.bind" % "jaxb-api" % "2.3.1"
   val ScalacheckGenRegexp = "wolfendale" %% "scalacheck-gen-regexp" % "0.1.2"
   val UUIDCreator = "com.github.f4b6a3" % "uuid-creator" % "3.6.0"
-  val GraalSvm = "org.graalvm.nativeimage" % "svm" % "21.0.0.2"
+  val GraalSvm = "org.graalvm.nativeimage" % "svm" % "21.1.0"
   val CatsRetry = "com.github.cb372" %% "cats-retry" % "2.1.0"
 
   object Borer {
