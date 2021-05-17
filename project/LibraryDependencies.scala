3a6fb748f7f82ebce091d730943e8ac361a73d59
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-17 08:34:57 +0200

    Update http4s-blaze-server, http4s-circe, ... to 0.21.23 (#67)

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -49,7 +49,7 @@ object LibraryDependencies {
   }
 
   object Http4s {
-    val http4sVersion = "0.21.22"
+    val http4sVersion = "0.21.23"
     val blazeServer = "org.http4s" %% "http4s-blaze-server" % http4sVersion
     val circe = "org.http4s" %% "http4s-circe" % http4sVersion
     val dsl = "org.http4s" %% "http4s-dsl" % http4sVersion
