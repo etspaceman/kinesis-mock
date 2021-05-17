92aa9f1cfbd18a90b9e65e04e0b7373dcfb60537
Scala Steward <me@scala-steward.org>
2021-05-17 23:01:03 +0200

    Update log4cats-slf4j to 2.1.1

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -20,7 +20,7 @@ object LibraryDependencies {
   }
 
   object Log4Cats {
-    val log4CatsVersion = "1.3.1"
+    val log4CatsVersion = "2.1.1"
     val slf4j = "org.typelevel" %% "log4cats-slf4j" % log4CatsVersion
   }
 
