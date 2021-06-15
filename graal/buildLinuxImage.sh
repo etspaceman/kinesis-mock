e6af57945003c4d464b3cf5d691e5b1e4a8acf20
Eric Meisel <eric.steven.meisel@gmail.com>
2021-06-15 00:11:52 -0500

    Use current docker image for linux executable

---
+++ graal/buildLinuxImage.sh
@@ -0,0 +1,4 @@
+VERSION=$(cat version.sbt | cut -d '"' -f2)
+
+mkdir shared && \
+docker run -it --rm -v ${PWD}/shared:/shared ghcr.io/etspaceman/kinesis-mock:${VERSION} cp /opt/kinesis-mock /shared/kinesis-mock
