4cf27df6e8751e8ee641a7c29a04e1d9634731ae
Eric Meisel <eric.steven.meisel@gmail.com>
2021-06-14 23:12:58 -0500

    More fixes

--- graal/buildNativeImageMac.sh
+++ graal/buildNativeImageMac.sh
@@ -3,7 +3,6 @@ OUTPUT_FILE=${OUTPUT_FILE:-kinesis-mock-native}
 
 native-image \
     --no-server \
-    -H:+StaticExecutableWithDynamicLibC \
     -J-Xmx7G \
     --no-fallback \
     --verbose \
---
+++ graal/installStaticDependencies.sh
@@ -0,0 +1,20 @@
+RESULT_LIB="staticlibs"
+
+sudo apt-get install libstdc++6
+
+mkdir ${RESULT_LIB} && \
+    curl -L -o musl.tar.gz https://musl.libc.org/releases/musl-1.2.1.tar.gz && \
+    mkdir musl && tar -xvzf musl.tar.gz -C musl --strip-components 1 && cd musl && \
+    ./configure --disable-shared --prefix=${RESULT_LIB} && \
+    make && make install && \
+    cd / && rm -rf /muscl && rm -f /musl.tar.gz && \
+    cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ${RESULT_LIB}/lib/
+
+echo "${RESULT_LIB}/bin" >> $GITHUB_PATH
+echo "CC=musl-gcc" >> $GITHUB_ENV
+
+curl -L -o zlib.tar.gz https://zlib.net/zlib-1.2.11.tar.gz && \
+   mkdir zlib && tar -xvzf zlib.tar.gz -C zlib --strip-components 1 && cd zlib && \
+   ./configure --static --prefix=${RESULT_LIB} && \
+   make && make install && \
+   cd / && rm -rf /zlib && rm -f /zlib.tar.gz
