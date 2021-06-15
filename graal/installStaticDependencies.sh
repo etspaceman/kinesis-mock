723a5e013e2f24f230d00b1bf2e2f976aac96646
Eric Meisel <eric.steven.meisel@gmail.com>
2021-06-14 23:47:12 -0500

    please let this be the right path

--- graal/installStaticDependencies.sh
+++ graal/installStaticDependencies.sh
@@ -1,14 +1,12 @@
 RESULT_LIB="staticlibs"
 
-sudo apt-get install -y apt-file && apt-file update *&& apt-file find libstdc++.a
-
 mkdir ${RESULT_LIB} && \
     curl -L -o musl.tar.gz https://musl.libc.org/releases/musl-1.2.1.tar.gz && \
     mkdir musl && tar -xvzf musl.tar.gz -C musl --strip-components 1 && cd musl && \
     ./configure --disable-shared --prefix=${RESULT_LIB} && \
     make && make install && \
     cd / && rm -rf /muscl && rm -f /musl.tar.gz && \
-    cp /usr/lib/debug/libstdc++.a ${RESULT_LIB}/lib/
+    cp /usr/lib/gcc/x86_64-linux-gnu/9/libstdc++.a ${RESULT_LIB}/lib/
 
 echo "${RESULT_LIB}/bin" >> $GITHUB_PATH
 echo "CC=musl-gcc" >> $GITHUB_ENV
