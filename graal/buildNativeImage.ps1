19ca1dd0c9167957da071a8eac25a178a527dc81
Eric Meisel <eric.steven.meisel@gmail.com>
2021-06-14 20:34:20 -0500

    Use powershell for windows

---
+++ graal/buildNativeImage.ps1
@@ -0,0 +1,22 @@
+$JAR_FILE = $env:JAR_FILE ?? "kinesis-mock.jar";
+$OUTPUT_FILE = $env:OUTPUT_FILE ?? "kinesis-mock-native";
+
+native-image.cmd `
+    --no-server `
+    --static `
+    --libc=musl `
+    -J-Xmx7G `
+    --no-fallback `
+    --verbose `
+    --enable-all-security-services `
+    --enable-url-protocols=http,https `
+    --initialize-at-build-time=scala `
+    -H:ReflectionConfigurationFiles=graal\reflect-config.json `
+    -H:ResourceConfigurationFiles=graal\resource-config.json `
+    -H:+ReportExceptionStackTraces `
+    -H:+AddAllCharsets `
+    --report-unsupported-elements-at-runtime `
+    --allow-incomplete-classpath `
+    --install-exit-handlers `
+    -jar $JAR_FILE `
+    $OUTPUT_FILE
