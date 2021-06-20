b8064c968b722218d6e36dedcf2e983b1534c074
Eric Meisel <eric.steven.meisel@gmail.com>
2021-06-20 10:32:46 -0500

    DescribeStream functional tests were running the wrong API calls

--- src/fun/scala/kinesis/mock/DescribeStreamSummaryTests.scala
+++ src/fun/scala/kinesis/mock/DescribeStreamSummaryTests.scala
@@ -2,6 +2,7 @@ package kinesis.mock
 
 import java.util.Collections
 
+import cats.syntax.all._
 import software.amazon.awssdk.services.kinesis.model._
 
 class DescribeStreamSummaryTests
@@ -35,6 +36,22 @@ class DescribeStreamSummaryTests
     } yield assert(
       res.streamDescriptionSummary == expected,
       s"${res.streamDescriptionSummary()}\n$expected"
+    )
+  }
+
+  fixture.test("It should describe initialized streams") { resources =>
+    for {
+      res <- initializedStreams.parTraverse { case (name, _) =>
+        describeStreamSummary(resources.kinesisClient, name).map(
+          _.streamDescriptionSummary()
+        )
+      }
+    } yield assert(
+      res.map(s =>
+        s.streamName() -> s.openShardCount()
+      ) == initializedStreams &&
+        res.forall(s => s.streamStatus() == StreamStatus.ACTIVE),
+      s"$res"
     )
   }
 }
