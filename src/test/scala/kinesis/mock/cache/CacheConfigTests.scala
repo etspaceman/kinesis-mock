b20fe833831f1aa8f179b0f1370e96ec27bba3e2
Eric Meisel <eric.steven.meisel@gmail.com>
2021-09-09 16:40:04 -0500

    Coverage around loading regions in INITIALIZE_STREAMS

--- src/test/scala/kinesis/mock/cache/CacheConfigTests.scala
+++ src/test/scala/kinesis/mock/cache/CacheConfigTests.scala
@@ -1,11 +1,12 @@
 package kinesis.mock.cache
 
 import enumeratum.scalacheck._
-import org.scalacheck.{Prop, Test}
+import org.scalacheck.{Gen, Prop, Test}
 
 import kinesis.mock.api.CreateStreamRequest
 import kinesis.mock.instances.arbitrary._
 import kinesis.mock.models.{AwsRegion, StreamName}
+import kinesis.mock.syntax.scalacheck._
 
 class CacheConfigTests
     extends munit.CatsEffectSuite
@@ -53,6 +54,43 @@ class CacheConfigTests
           CreateStreamRequest(2, streamName2),
           CreateStreamRequest(1, streamName3)
         )
+      )
+
+      assert(res == Right(expected), s"$res")
+  })
+
+  test(
+    "It should properly parse valid INITIALIZE_STREAMS string with multiple streams and regions"
+  )(Prop.forAll {
+    (
+        streamName1: StreamName,
+        streamName2: StreamName,
+        streamName3: StreamName,
+        streamName4: StreamName,
+        awsRegion: AwsRegion
+    ) =>
+      val testRegion1 =
+        Gen.oneOf(AwsRegion.values.filterNot(_ == awsRegion)).one
+      val testRegion2 = Gen
+        .oneOf(
+          AwsRegion.values.filterNot(x => x == awsRegion && x == testRegion1)
+        )
+        .one
+      val res = CacheConfig
+        .initializeStreamsReader(
+          awsRegion,
+          s"$streamName1:3:${testRegion1.entryName},$streamName2:2,$streamName3:1:${testRegion1.entryName},$streamName4:1:${testRegion2.entryName}"
+        )
+
+      val expected = Map(
+        awsRegion -> List(
+          CreateStreamRequest(2, streamName2)
+        ),
+        testRegion1 -> List(
+          CreateStreamRequest(3, streamName1),
+          CreateStreamRequest(1, streamName3)
+        ),
+        testRegion2 -> List(CreateStreamRequest(1, streamName4))
       )
 
       assert(res == Right(expected), s"$res")
