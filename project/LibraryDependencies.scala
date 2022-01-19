e99d9d56bcac61c2e9296aab302581a99b5052de
Scala Steward <me@scala-steward.org>
2022-01-19 04:36:04 +0100

    Update svm to 22.0.0

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -10,7 +10,7 @@ object LibraryDependencies {
   val ScalacheckGenRegexp =
     "io.github.wolfendale" %% "scalacheck-gen-regexp" % "0.1.3"
   val UUIDCreator = "com.github.f4b6a3" % "uuid-creator" % "4.3.1"
-  val GraalSvm = "org.graalvm.nativeimage" % "svm" % "21.3.1"
+  val GraalSvm = "org.graalvm.nativeimage" % "svm" % "22.0.0"
   val CatsRetry = "com.github.cb372" %% "cats-retry" % "3.1.0"
   val OsLib = "com.lihaoyi" %% "os-lib" % "0.8.0"
 
