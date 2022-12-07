924e9df55a59c277847002ff080447aea15c1a71
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-12-07 03:45:05 +0100

    Update cats-effect to 3.4.2 (#383)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -44,7 +44,7 @@ object LibraryDependencies {
 
   object Cats {
     val catsVersion = "2.9.0"
-    val catsEffectVersion = "3.4.1"
+    val catsEffectVersion = "3.4.2"
     val core = "org.typelevel" %% "cats-core" % catsVersion
     val effect = "org.typelevel" %% "cats-effect" % catsEffectVersion
   }
