1e2fe42b87c30028404df74c1e1436cde5abb16a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-07 18:58:11 +0100

    Update kinesis, utils to 2.19.12 (#398)

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -35,7 +35,7 @@ object LibraryDependencies {
   }
 
   object Aws {
-    val sdkVersion = "2.19.10"
+    val sdkVersion = "2.19.12"
     val utils = "software.amazon.awssdk" % "utils" % sdkVersion
     val kinesis = "software.amazon.awssdk" % "kinesis" % sdkVersion
     val kpl = "com.amazonaws" % "amazon-kinesis-producer" % "0.14.13"
