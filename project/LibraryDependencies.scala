ee7fdc09e3530afcf1b127e38c29a576efd50e59
Scala Steward <scala_steward@virtuslab.com>
2023-02-08 15:19:02 +0000

    Update kinesis, utils to 2.19.33

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -35,7 +35,7 @@ object LibraryDependencies {
   }
 
   object Aws {
-    val sdkVersion = "2.19.31"
+    val sdkVersion = "2.19.33"
     val utils = "software.amazon.awssdk" % "utils" % sdkVersion
     val kinesis = "software.amazon.awssdk" % "kinesis" % sdkVersion
     val kpl = "com.amazonaws" % "amazon-kinesis-producer" % "0.15.2"
