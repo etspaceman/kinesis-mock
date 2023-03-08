e3a077558effc597b15cb3b15538e578f1a94352
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-03-08 15:58:07 +0100

    Update os-lib to 0.9.1 (#445)

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -12,7 +12,7 @@ object LibraryDependencies {
   val UUIDCreator = "com.github.f4b6a3" % "uuid-creator" % "5.2.0"
   val GraalSvm = "org.graalvm.nativeimage" % "svm" % "22.3.1"
   val CatsRetry = "com.github.cb372" %% "cats-retry" % "3.1.0"
-  val OsLib = "com.lihaoyi" %% "os-lib" % "0.9.0"
+  val OsLib = "com.lihaoyi" %% "os-lib" % "0.9.1"
 
   object Borer {
     val borerVersion = "1.8.0"
