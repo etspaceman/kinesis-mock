e31d79c6769806268688426ddc92bdac2d858606
Eric Meisel <eric.steven.meisel@gmail.com>
2023-07-07 15:53:11 -0500

    Remove

--- graal/buildLinuxImage.sh
+++
@@ -1,4 +0,0 @@
-VERSION=$(cat version.sbt | cut -d '"' -f2)
-
-mkdir shared && \
-docker run --rm -v ${PWD}/shared:/shared ghcr.io/etspaceman/kinesis-mock:${VERSION} cp /opt/kinesis-mock /shared/kinesis-mock
