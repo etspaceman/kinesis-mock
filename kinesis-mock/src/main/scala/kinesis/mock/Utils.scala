d67c8acfe96b07ad88de65e308cccd562f7dd914
Eric Meisel <eric.meisel@siriusxm.com>
2025-01-22 15:17:34 -0600

    Use CE 3.6.0-RC1

--- kinesis-mock/src/main/scala/kinesis/mock/Utils.scala
+++ kinesis-mock/src/main/scala/kinesis/mock/Utils.scala
@@ -4,11 +4,16 @@ import java.time.Instant
 
 import cats.effect.IO
 import cats.effect.SyncIO
+import cats.effect.std.SecureRandom
 import cats.effect.std.UUIDGen
 
 object Utils {
-  def randomUUID = UUIDGen.randomUUID[SyncIO].unsafeRunSync()
-  def randomUUIDString = UUIDGen.randomString[SyncIO].unsafeRunSync()
+  private implicit val syncIOSecureRandom =
+    SecureRandom.javaSecuritySecureRandom[SyncIO].unsafeRunSync()
+  def randomUUID =
+    UUIDGen.randomUUID[SyncIO].unsafeRunSync()
+  def randomUUIDString =
+    UUIDGen.randomString[SyncIO].unsafeRunSync()
   def md5(bytes: Array[Byte]): Array[Byte] = MD5.compute(bytes)
   def now = IO.realTime.map(d => Instant.EPOCH.plusNanos(d.toNanos))
 }
